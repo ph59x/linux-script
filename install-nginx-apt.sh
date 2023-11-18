1cffc2af01f7620a3045441e0a0c4798cd97eb61
ph59x <YmoonL@outlook.com>
2023-11-18 20:48:02 +0800

    Add nginx auto install script
    for Debian / Ubuntu (apt based)
    commands from nginx document

---
+++ install-nginx-apt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt install -y curl gnupg2 ca-certificates lsb-release debian-archive-keyring
+
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+https://nginx.org/packages/debian `lsb_release -cs` nginx" \
+    | tee /etc/apt/sources.list.d/nginx.list
+
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+    | tee /etc/apt/preferences.d/99nginx
+
+apt update
+
+apt install nginx
